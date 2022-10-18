fd0b743130b3336fb1a4f35eb9ce78eccb4e9dc5
KSY173 <112866329+KSY173@users.noreply.github.com>
2022-10-18 16:10:42 +0900

    practice6-1

--- main.c
+++ main.c
@@ -3,26 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void sub(void);
+int all_files;
 
-int main(void) {
-	
-	int i;
-	
-	for(i=0; i<3; i++)
-		sub();
-		
-	return 0;
-}
+extern void sub();
 
-void sub(void)	
+int main(void)
 {
-	int auto_count = 0;
-	static int static_count = 0;
+	sub();
+	printf("%d\n", all_files);
 	
-	auto_count++;
-	static_count++;
-	
-	printf("auto_count = %d\n", auto_count);
-	printf("static_count = %d\n", static_count);
+	return 0;
 }
