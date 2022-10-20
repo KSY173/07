d84ae0edb3e26464a1385647430ddcb9f86db603
KSY173 <112866329+KSY173@users.noreply.github.com>
2022-10-20 23:38:13 +0900

    practice4-2

--- main.c
+++ main.c
@@ -3,14 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-static int all_files;
-
-extern void sub();
+int i;
 
 int main(void)
 {
-	sub();
-	printf("%d\n", all_files);
-	
+	for(i = 0; i<5; i++)
+	{
+		f();
+	}
 	return 0;
+}
+
+void f(void)
+{
+	for(i = 0; i<10; i++)
+		printf("#");
 }
